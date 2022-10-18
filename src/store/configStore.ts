ece6a86945fcd52978194eba3361cbfb06c4c95b
김지호 <crsn1111@gmail.com>
2022-10-18 19:51:08 +0900

    [feat] middleware setting

--- src/store/configStore.ts
+++ src/store/configStore.ts
@@ -1,18 +1,22 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import counterSlice from "./slice/counterSlice";
+import profileSlice from "./slice/profileSlice";
 
 const rootReducer = combineReducers({
   counter: counterSlice,
+  profile: profileSlice,
 });
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
