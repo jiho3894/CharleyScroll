fd13f73807ac0cd132da15c1162cc02e2faf91b3
김지호 <crsn1111@gmail.com>
2022-10-18 19:50:42 +0900

    [feat] crud 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,77 @@
-import styled from "@emotion/styled";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../store/configStore";
+import { AppDispatch, RootState } from "../store/configStore";
 import { addNumber, minusNumber } from "../store/slice/counterSlice";
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: skyblue;
-  button {
-    width: 40px;
-    height: 40px;
-    font-size: 20px;
-  }
-`;
+import {
+  Profile,
+  __delProfile,
+  __getProfile,
+  __postProfile,
+} from "../store/slice/profileSlice";
+import { Box, BoxContainer, Button, Container, Input } from "./style";
 
 const Home = () => {
-  const dispatch = useDispatch();
+  const [text, setText] = useState<string>("");
+  const dispatch = useDispatch<AppDispatch>();
   const { val } = useSelector((state: RootState) => state.counter);
-  console.log(val);
+  const { profile, isLoading } = useSelector(
+    (state: RootState) => state.profile
+  );
+  useEffect(() => {
+    dispatch(__getProfile());
+  }, [dispatch]);
   const Plus = () => {
     dispatch(addNumber(1));
   };
   const Minus = () => {
     dispatch(minusNumber(1));
   };
+  const ChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const postProfile = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: Profile = {
+      name: text,
+      id: String(profile.length + 1),
+      avatar: "hi",
+    };
+    dispatch(__postProfile(data));
+  };
+  const deleteProfile = (id: string) => {
+    dispatch(__delProfile(id));
+  };
   return (
     <Container>
-      <div>{val}</div>
-      <button onClick={Plus}>+</button>
-      <button onClick={Minus}>-</button>
+      <form onSubmit={postProfile}>
+        <div>{val}</div>
+        <Button type="button" onClick={Plus}>
+          +
+        </Button>
+        <Button type="button" onClick={Minus}>
+          -
+        </Button>
+        <Input
+          autoFocus
+          type="text"
+          placeholder="이름 입력"
+          onChange={ChangeText}
+        />
+        {!isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <BoxContainer>
+            {profile.map((data) => {
+              return (
+                <Box key={data.id} onClick={() => deleteProfile(data.id)}>
+                  <span>{data.name}</span>
+                  <img src={data.avatar} alt="" />
+                </Box>
+              );
+            })}
+          </BoxContainer>
+        )}
+      </form>
     </Container>
   );
 };
